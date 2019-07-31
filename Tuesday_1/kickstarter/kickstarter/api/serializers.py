1601991c08be685bf07b70055018fc960a037929
StphnRssll <stephendonrussell@gmail.com>
2019-07-30 21:04:28 -0400

    Added comments

--- Tuesday_1/kickstarter/kickstarter/api/serializers.py
+++ Tuesday_1/kickstarter/kickstarter/api/serializers.py
@@ -1,8 +1,9 @@
 from rest_framework import serializers
 from api.models import KickstarterCampaign
 
+# This serializer calls on Django's ModelSerializer to break the campaigns into its 5 fields 
+
 class ApiSerializer(serializers.ModelSerializer):
     class Meta:
         model = KickstarterCampaign
         fields = ['id','backers','blurb','pledged','created']
-        
